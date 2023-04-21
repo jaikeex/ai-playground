0f341457ad836243859fc44e718ae55b8c494e65
Jakub Hrubý <hrubyy.jakub@gmail.com>
2023-04-21 14:20:05 +0200

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Hero } from 'components/Hero';
+import { Demo } from 'components/Demo';
 
-export const App = () => <div />;
+export const App = () => (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
